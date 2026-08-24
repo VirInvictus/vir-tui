a43f30e5e87ae65a828cf507aef4dcf40f04f640
Brandon LaRocque <larocque.brandon@gmail.com>
2026-08-24 11:17:39 -0400

    Clean up python scratch scripts

--- fix_init.py
+++
@@ -1,9 +0,0 @@
-import re
-with open("src/vir_tui/__init__.py", "r") as f:
-    text = f.read()
-
-text = re.sub(r'# for backwards compatibility', '', text)
-
-with open("src/vir_tui/__init__.py", "w") as f:
-    f.write(text)
-
